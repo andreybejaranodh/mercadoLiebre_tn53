8a37ef60ad1880e125454e7f625576f68c8bd714
Andrey Bejarano <andreybejarano@gmail.com>
2023-02-07 23:51:17 -0300

    config express port

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.get('/register', (req, res) => {
     res.sendFile(path.join(__dirname, '/views/register.html'));
 });
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log(`Server started on http://localhost:${port}`);
 });
